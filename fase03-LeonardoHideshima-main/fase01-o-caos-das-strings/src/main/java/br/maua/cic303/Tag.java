1e43f489875a354a8a7c064b1e1f85b4cd59b1df
LEONARDO KAZUSHI HIDESHIMA <162127939+LeonardoHideshima@users.noreply.github.com>
2026-06-15 13:52:02 -0300

    Delete fase03-LeonardoHideshima-main directory

--- fase03-LeonardoHideshima-main/fase01-o-caos-das-strings/src/main/java/br/maua/cic303/Tag.java
+++
@@ -1,5 +0,0 @@
-package br.maua.cic303;
-
-public enum Tag {
-    ID, NUMBER, ASSIGN, ADD_OP, MUL_OP, EOF, ERROR
-}
--- fase03-LeonardoHideshima-main/fase04-dando-significado/src/main/java/br/maua/cic303/ast/ASTNode.java
+++
@@ -1,6 +0,0 @@
-package br.maua.cic303.ast;
-
-public abstract class ASTNode {
-    // Para a Fase 5, aqui entrará o método abstract double avaliar(Environment
-    // env);
-}
